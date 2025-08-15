03ccadade8cf8838f2725e775a07638252d3cce5
Batubenga0B74 <freudymena84@gmail.com>
2025-08-15 20:37:26 +1100

    so musicas do C4

--- src/app/lib/deezer.ts
+++ src/app/lib/deezer.ts
@@ -21,7 +21,7 @@ interface DeezerChartResponse {
 export async function getPopularTracks(): Promise<DeezerTrack[]> {
   try {
     const response = await axios.get<DeezerChartResponse>(
-      "https://api.deezer.com/chart/0/tracks"
+      " https://api.deezer.com/search/track?q=C4%20Pedro"
     );
     return response.data.data; // retorna só a lista de músicas
   } catch (error) {
