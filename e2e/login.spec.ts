0f347e301f0546b9e1f41a1f7f7e8af8324d5ff6
Batubenga0B74 <freudymena84@gmail.com>
2025-09-02 16:58:19 +0100

    add : test loginpage

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -17,5 +17,44 @@ test('test de login com dados validos', async ({ page }) => {
   await page.locator('div').filter({ hasText: 'The music platform empowering' }).nth(1).click();
 1
 });
+test('test com o senha invalida', async ({ page }) => {
+
+    const login = LoginPage(page)
+   
+       await login.openSite()
+   
+       await login.submitForm({...playload , senha:"oncler2027"})
+    //    await page.getByText('Email ou senha inválidos').click();
+       await expect(page.getByText("Email ou senha inválidos")).toBeVisible();
 
 
+   
+   });
+   test('test com o email invalida', async ({ page }) => {
+
+    const login = LoginPage(page)
+   
+       await login.openSite()
+   
+       await login.submitForm({...playload , email:"oncler2027"})
+    //    await page.getByText('Email ou senha inválidos').click();
+       await expect(page.getByText("Email ou senha inválidos")).toBeVisible();
+   
+   });
+
+
+   test('test', async ({ page }) => {
+    await page.goto('https://oncler.netlify.app/login');
+    await page.getByRole('textbox', { name: 'Email' }).click();
+    await page.getByRole('textbox', { name: 'Email' }).fill('testoncler@gmail.com');
+    await page.getByRole('textbox', { name: 'Palavra passe' }).click();
+    await page.getByRole('textbox', { name: 'Palavra passe' }).fill('oncler2025');
+    await page.getByRole('button', { name: 'Acessar' }).click();
+    await page.goto('https://oncler.netlify.app/login');
+    await page.getByRole('textbox', { name: 'Email' }).click();
+    await page.getByRole('textbox', { name: 'Email' }).fill('testoncler@gmail.com');
+    await page.getByRole('textbox', { name: 'Palavra passe' }).click();
+    await page.getByRole('textbox', { name: 'Palavra passe' }).fill('oncler123');
+    await page.getByRole('button', { name: 'Acessar' }).click();
+    await page.getByText('Email ou senha inválidos').click();
+  });
