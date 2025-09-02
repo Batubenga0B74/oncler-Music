6a7cb978865eb3788ca312e2e39772d3f5f94e01
Batubenga0B74 <freudymena84@gmail.com>
2025-09-02 16:46:55 +0100

    add : test loginpage

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -2,13 +2,17 @@
 import { test, expect } from '@playwright/test';
 import {LoginPage}from "../support/fixtures/pages/login"
 const playload = {
-    email: "estoncler@gmail.com",
+    email: "testoncler@gmail.com",
     senha: "oncler2025"
 }
 test('test de login com dados validos', async ({ page }) => {
+
  const login = LoginPage(page)
+
     await login.openSite()
+
     await login.submitForm(playload)
+
   await expect(page).toHaveURL("https://oncler.netlify.app/home");
   await page.locator('div').filter({ hasText: 'The music platform empowering' }).nth(1).click();
 1
